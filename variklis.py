f825199f8570eca745e21b0805cd40b5843417df
Sdf6e <laimisoertelis@gmail.com>
2022-04-19 10:04:02 +0300

    duomenu bazes struktura padaryta

--- variklis.py
+++ variklis.py
@@ -1,1 +1,42 @@
-from tkinter import *
+from sqlalchemy import Column, Integer, String, VARCHAR, Float, DateTime, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+engine = create_engine('sqlite:///C:/Users/laimi/Desktop/musubiudzetas/duomenys.db')
+base = declarative_base()
+
+class Mainwallet(base):
+    __tablename__ = "Mainwallet"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    use = Column("USE", VARCHAR)
+    amount = Column("AMOUNT", Float)
+    from_to = Column("FROM / TO", VARCHAR)
+    date_when = Column("DATE", DateTime, default=datetime.utcnow())
+
+    def __init__(self, use, amount, from_to, date_when):
+        self.use = use
+        self.amount = amount
+        self.from_to = from_to
+        self.date_when = date_when
+
+    def __repr__(self):
+        return f'|Mainwallet| {self.id} : {self.use} | {self.amount} | {self.from_to} | {self.date_when}'
+
+class Savingswallet(base):
+    __tablename__ = "Savingswallet"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    use = Column("USE", VARCHAR)
+    amount = Column("AMOUNT", Float)
+    from_to = Column("FROM / TO", VARCHAR)
+    date_when = Column("DATE", DateTime, default=datetime.utcnow())
+
+    def __init__(self, use, amount, from_to, date_when):
+        self.use = use
+        self.amount = amount
+        self.from_to = from_to
+        self.date_when = date_when
+
+    def __repr__(self):
+        return f'|Savingswallet| {self.id} : {self.use} | {self.amount} | {self.from_to} | {self.date_when}'
+
+base.metadata.create_all(engine)
